f78040e86098d1943009204ac478c973812f772a
genevievepoint <genevieve.point@my.jcu.edu.au>
2015-11-06 16:58:07 +1000

    Removed redundant functions from the app module, also added in some comments so I can remember what I did and why. Also reformatted the code to make it appear neater.

--- app.py
+++ app.py
@@ -1,39 +1,42 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.core.window import Window
-from currency import get_all_details
-from currency import convert
-from trip import Details
 import datetime
 import time
 
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivy.core.window import Window
+
+from currency import get_all_details
+from currency import convert
+from trip import Details
 
 
 # The custom App class
 class CurrencyConvert(App):
-
     def __init__(self, **kwargs):
         super(CurrencyConvert, self).__init__(**kwargs)
         self.details = Details()
 
-    def build(self,):
+# Creates the window and provides the basic information used in the start up of the program
+    def build(self, ):
         Window.size = (350, 700)
         self.title = "Foreign Exchange Calculator"
         self.root = Builder.load_file('gui.kv')
         self.find_trip_details()
+        # Sets the current date in the layout
         self.root.ids.current_date.text = 'Today is: \n' + str(datetime.date.today()).replace('-', '/')
+        # sets the current location of the user in their trip
         self.root.ids.trip_location.text = ('Current Location: \n' + self.details.current_country(str(datetime.date.today()).replace('-', '/')))
         return self.root
 
-    def add_date(self, start_date, end_date):
-        self.start_date = start_date
-        self.end_date = end_date
-
     def find_trip_details(self):
+        # calls the Details class from the trip module
         self.details = Details()
+        # opens the config file that contains the users trip details
         file = open('config.txt', encoding='utf-8')
         self.home_country = file.readline().strip()
         self.root.ids.home_country.text = str(self.home_country)
+        # displays the users trip order in a chronological list
         self.country_list = []
         for line in file:
             parts = line.strip().split(',')
@@ -43,22 +46,21 @@ class CurrencyConvert(App):
             self.country_list.append(parts[0])
         file.close()
         return self.country_list
-        self.place_dictionary = get_all_details()
-        self.home_currency = self.place_dictionary[self.home_country][1]
 
-    def current_location(self, current_location):
-        self.current_location = current_location
-        return current_location
 
     def get_currency_conversion(self, directions):
         # print(directions)
+        # creates a dictionary from the currency module
         place_dictionary = get_all_details()
+        # stores the user selection from the spinner gui
         spinner_location = self.root.ids.country_selection.text
         # print(spinner_location)
         location_currency = place_dictionary[spinner_location][1]
         home_currency = place_dictionary[self.home_country][1]
         # print(location_currency)
         # print(home_currency)
+        # provides the status message and conversion amount in the gui
+        # allows for back and forth conversion
         if directions == 'to home':
             value = convert(float(self.root.ids.target_amount.text), home_currency, location_currency)
             self.root.ids.home_amount.text = str(value)
@@ -68,11 +70,12 @@ class CurrencyConvert(App):
             self.root.ids.target_amount.text = str(value)
             self.root.ids.status.text = home_currency + ' to ' + location_currency
 
+    # updates the conversion rate for the user
     def update_currency(self):
         self.root.update_currency.text = 'updated' + time.strftime('%X')
 
 
-#create and start the app
+# create and start the app
 CurrencyConvert().run()
 
 
